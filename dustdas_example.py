dd98f46a6715d6d29473bce9cdfae3127bfb957b
gglyptodon <janina.mass@gmail.com>
2018-04-13 12:25:28 +0200

    working example with eg json and fasta output for mrna

--- dustdas_example.py
+++ dustdas_example.py
@@ -6,7 +6,7 @@ from dustdas import gffhelper as gh
 from dustdas import fastahelper as fh
 
 
-def formatHelp():
+def format_help():
     print("""
         from http://www.ensembl.org/info/website/upload/gff.html:
     
@@ -32,8 +32,8 @@ def main():
     allthefeatures = {}
 
     genes = []
-    five_prime_UTRs = []
-    three_prime_UTRs = []
+    five_prime_utrs = []
+    three_prime_utrs = []
     mrnas = []
     exons = []
     cds = []
@@ -57,108 +57,85 @@ def main():
             exons.append(o)
 
         if o.feature == "five_prime_UTR":
-            five_prime_UTRs.append(o)
+            five_prime_utrs.append(o)
 
         if o.feature == "three_prime_UTR":
-            three_prime_UTRs.append(o)
-
+            three_prime_utrs.append(o)
 
     print(allthefeatures)
 
     # show first few genes
-    for g in genes[0:0]:
+    for g in genes[0:2]:
         print("gene:", g)
         print("mRNA:")
         # print all mRNAs whose "ID" tag value of mrna starts with "Name" tag value of gene
-        print ([m for m in mrnas if m.attrib_filter_fun(tfun= lambda x,y: x==y, targ = "ID", vfun = lambda x,y: x.startswith(y), varg=[a.value for a in g.attributes if a.tag=="Name"][0])])
+        print([m for m in mrnas if
+               m.attrib_filter_fun(tfun=lambda x, y: x == y, targ="ID", vfun=lambda x, y: x.startswith(y),
+                                   varg=[a.value for a in g.attributes if a.tag == "Name"][0])])
         print("exons:")
-        print ([(e.start,e.end,e.strand,e)  for e in exons if e.attrib_filter_fun(tfun= lambda x,y: x==y, targ = "ID", vfun = lambda x,y: x.startswith(y), varg=[a.value for a in g.attributes if a.tag=="Name"][0])])
+        print([(e.start, e.end, e.strand, e) for e in exons if
+               e.attrib_filter_fun(tfun=lambda x, y: x == y, targ="ID", vfun=lambda x, y: x.startswith(y),
+                                   varg=[a.value for a in g.attributes if a.tag == "Name"][0])])
         print("cds:")
-        print ([(e.start,e.end,e.strand)  for e in cds if e.attrib_filter_fun(tfun= lambda x,y: x==y, targ = "ID", vfun = lambda x,y: x.startswith(y), varg=[a.value for a in g.attributes if a.tag=="Name"][0])])
+        print([(e.start, e.end, e.strand) for e in cds if
+               e.attrib_filter_fun(tfun=lambda x, y: x == y, targ="ID", vfun=lambda x, y: x.startswith(y),
+                                   varg=[a.value for a in g.attributes if a.tag == "Name"][0])])
         print("3pUTR:")
-        print ([(e.start,e.end,e.strand)  for e in three_prime_UTRs if e.attrib_filter_fun(tfun= lambda x,y: x==y, targ = "ID", vfun = lambda x,y: x.startswith(y), varg=[a.value for a in g.attributes if a.tag=="Name"][0])])
+        print([(e.start, e.end, e.strand) for e in three_prime_utrs if
+               e.attrib_filter_fun(tfun=lambda x, y: x == y, targ="ID", vfun=lambda x, y: x.startswith(y),
+                                   varg=[a.value for a in g.attributes if a.tag == "Name"][0])])
         print("5pUTR:")
-        print ([(e.start,e.end,e.strand)  for e in five_prime_UTRs if e.attrib_filter_fun(tfun= lambda x,y: x==y, targ = "ID", vfun = lambda x,y: x.startswith(y), varg=[a.value for a in g.attributes if a.tag=="Name"][0])])
+        print([(e.start, e.end, e.strand) for e in five_prime_utrs if
+               e.attrib_filter_fun(tfun=lambda x, y: x == y, targ="ID", vfun=lambda x, y: x.startswith(y),
+                                   varg=[a.value for a in g.attributes if a.tag == "Name"][0])])
 
-        print ("#########")
+        print("#########")
 
     fasta_dict = fh.FastaParser.read_fasta_whole(args.fasta)
-    #for m in mrnas[0:3]:
-    #    identifier = m.seqname # this will be used to match the (genome) fasta sequence
-        # for cds or protein fasta this could be sth like: identifier = m.attrib_filter(tag="pacid")
-        # and regex pattern would be sth like this: r = r"""(.*)pacid={}""".format(identifier[0].value)
 
-        # matching fasta header starts with identifier
-    #    r = r"""^{}""".format(identifier)
+    def setupseq(gffobj, fastadct, regex):
 
-        # attach fasta sequence to object
-    #    h,s = m.get_sequence(fastadct=fasta_dict, regex=r)
-    #    mrnaseq=fh.FastaParser.get_sequence_by_coordinates(orig_seq=s,start=m.start, end=m.end, strand=m.strand)
-    #    m.attach_fasta(";".join([h,m.start, m.end, m.strand]),mrnaseq)
+        """
 
-
-        # look at all exons of mrna
-        # filter for all exons whose parent tag value starts with mrna ID tag
-    #    mrnaExons = [e for e in exons if e.attrib_filter_fun(tfun= lambda x,y: x==y, targ = "Parent", vfun = lambda x,y: x.startswith(y), varg=[a.value for a in m.attributes if a.tag=="ID"][0])]
-
-
-     #   print ("#")
-
-    def setupseq(gffobj, fastadct,regex, includeprotein):
-        # this is bs... looks like cds need to be concatenated first
-        #if not gffobj.feature in ["cds","CDS"]: #todo
-        #    print("{} might not be translatable to protein sequence".format(gffobj), file=sys.stderr)
-
-        id = gffobj.seqname
-        r = regex.format(id)
-        h,s = gffobj.get_sequence(fastadct=fastadct, regex=r)
-        seq=fh.FastaParser.get_sequence_by_coordinates(orig_seq=s,
-                                                          start=gffobj.start,
-                                                          end=gffobj.end,
-                                                          strand=gffobj.strand)
+        :param fastadct: dict
+        :param regex: rawstring for regex
+        :type gffobj: GFFObject
+        """
+        ident = gffobj.seqname
+        r = regex.format(ident)
+        h, s = gffobj.get_sequence(fastadct=fastadct, regex=r)
+        seq = fh.FastaParser.get_sequence_by_coordinates(orig_seq=s,
+                                                         start=gffobj.start,
+                                                         end=gffobj.end,
+                                                         strand=gffobj.strand)
         try:
-            gffobj.attach_fasta(";".join([h,gffobj.start,
-                                      gffobj.end,
-                                      gffobj.strand,
-                                      gffobj.frame]),
-                            seq,
-                            include_protein=True)
+            gffobj.attach_fasta(";".join([h, gffobj.start,
+                                          gffobj.end,
+                                          gffobj.strand,
+                                          gffobj.frame]),
+                                seq)
         except fh.SequenceTranslationException as e:
             print("{}, skipping {}".format(e, gffobj), file=sys.stderr)
-    #for c in cds:
-    #    id = c.seqname
-    #    r = r"""^{}""".format(id)
-    #    h,s = c.get_sequence(fastadct=fasta_dict, regex=r)
-    #    cdsseq=fh.FastaParser.get_sequence_by_coordinates(orig_seq=s,start=c.start, end=c.end, strand=c.strand)
-    #    c.attach_fasta(";".join([h,c.start, c.end, c.strand]),cdsseq, include_protein=True)
-    #    print(">{}".format(c.fasta_header))
-    #    print(fh.SeqTranslator.dna2prot(c.fasta_sequence, frameshift=c.frame))
-
-    #for g in genes:
-    #    setupseq(g, fasta_dict, r"""^{}""", includeprotein=True)
 
     for c in cds:
-        #todo remove includeprotein
-        setupseq(c, fasta_dict, r"""^{}""", includeprotein=True)
-    with open ("cds.json", 'w') as out:
-        #for m in exons[0:3]:
-        #    out.write(m.to_json())
-        #for m in mrnas[0:3]:
-        #    out.write(m.to_json(omit_fasta=True))
-        #for m in mrnas[0:3]:
-        #    out.write(m.to_json(omit_fasta=False))
-        for c in cds:
-            out.write(c.to_json(omit_fasta_protein=False))
-
-    #with open ("genes.json",'w') as out:
-    #    for g in genes:
-    #        out.write(g.to_json(omit_fasta_protein=False))
-
-
-
-
-
-
+        setupseq(c, fasta_dict, r"^{}")
+    for m in mrnas[:11]:
+        identifier = [a.value for a in m.attributes if a.tag == "ID"][0]
+        # find all cds that belong to mrna
+        conc = ""
+        with open("{}_cds.json".format(identifier), 'w') as out:
+            mrna_cds = [c for c in cds if c.attrib_filter_fun(tfun=lambda x, y: x == y,
+                                                              targ="Parent",
+                                                              vfun=lambda x, y: x.startswith(y),
+                                                              varg=[a.value for a in m.attributes if a.tag == "ID"][0])
+                        ]
+            for c in mrna_cds:
+                out.write(c.to_json())
+                conc += c.fasta_sequence
+        with open("{}_cds_concatenated.fasta".format(identifier), 'w') as outfa:
+            outfa.write(">{}\n{}\n".format(identifier + "_cds", conc))
+        with open(">{}_cds_prot_concatenated.fasta".format(identifier), 'w') as outfa:
+            outfa.write("{}\n{}".format(identifier + "_cds_protein", fh.SeqTranslator.dna2prot(conc, frameshift=0)))
 
 
 if __name__ == "__main__":
