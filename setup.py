426a484deae0d6e980632259ac51326061a8dcff
gglyptodon <janina.mass@gmail.com>
2023-02-14 14:24:15 +0100

    update version number, update package info

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='dustdas',
-      version='0.1',
-      description='gff foo',
-      url='http://github.com/janinamass/dustdas',
+      version='0.2',
+      description='gff utils',
+      url='http://github.com/gglyptodon/dustdas',
       author='jmass',
       author_email='janina.mass@gmail.com',
       license='GPLv3',
